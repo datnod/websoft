5c283ddbbd3db2657cf43de18e42354fcdae0178
Liridon Smajli <vondony@hotmail.com>
2020-01-28 13:43:20 +0100

    Small steps for A03

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -2,9 +2,15 @@
  * A function to wrap it all in.
  */
 (function () {
-    "use strict";
+    'use strict';
 
-
-
-    console.log("All ready.");
+        fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1283')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                console.log(myJson);
+            });
+    
+        console.log('Sandbox MEGA is ready!');
 }());
