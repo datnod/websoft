c1cbd7aa5d93df4388dd5fb8b711240cb73bfcaa
Liridon Smajli <vondony@hotmail.com>
2020-02-08 17:53:32 +0100

    hej

--- example/javascript-core/pushing-the-ball/main.js
+++ example/javascript-core/pushing-the-ball/main.js
@@ -8,14 +8,14 @@ window.onload = function() {
         element = document.getElementById('flash'),
         myBall = {};
 
-    html  = "<p>Click inside this area to move the ball.</p>";
+    html = "<p>Click inside this area to move the ball.</p>";
     html += "<div id='ball' class='ball'></div>";
     element.innerHTML = html;
 
     myBall.HTMLelement = document.getElementById('ball');
     myBall.image = "http://dbwebb.se/img/black_ball_64_64.png";
-    myBall.position = {x: 10, y: 10};
-    myBall.dimension = {w: 64, h: 64};
+    myBall.position = { x: 10, y: 10 };
+    myBall.dimension = { w: 64, h: 64 };
 
 
 
@@ -32,13 +32,13 @@ window.onload = function() {
     /**
      * Set up the css-details for the ball.
      */
-    myBall.init = function () {
+    myBall.init = function() {
         console.log("init");
-        this.HTMLelement.style.position  = "absolute";
+        this.HTMLelement.style.position = "absolute";
         this.HTMLelement.style.backgroundImage = "url(" + this.image + ")";
-        this.HTMLelement.style.left   = this.position.x + "px";
-        this.HTMLelement.style.top    = this.position.y + "px";
-        this.HTMLelement.style.width  = this.dimension.w + "px";
+        this.HTMLelement.style.left = this.position.x + "px";
+        this.HTMLelement.style.top = this.position.y + "px";
+        this.HTMLelement.style.width = this.dimension.w + "px";
         this.HTMLelement.style.height = this.dimension.h + "px";
         this.HTMLelement.style.transition = "left 0.5s, top 0.5s";
         this.draw();
@@ -49,7 +49,7 @@ window.onload = function() {
     /**
      * Draw the ball.
      */
-    myBall.draw = function () {
+    myBall.draw = function() {
         console.log("draw");
         this.HTMLelement.style.left = this.position.x + "px";
         this.HTMLelement.style.top = this.position.y + "px";
@@ -61,7 +61,7 @@ window.onload = function() {
     /**
      * Move the ball.
      */
-    myBall.move = function (x, y) {
+    myBall.move = function(x, y) {
         console.log("move");
         this.position.x = x;
         this.position.y = y;
@@ -73,7 +73,7 @@ window.onload = function() {
     /**
      * Add event listener for mouse clicks.
      */
-    element.addEventListener('click', function (event) {
+    element.addEventListener('click', function(event) {
         console.log('Clicked on (client): ' + event.clientX + ' x ' + event.clientY);
         console.log('Clicked on (screen): ' + event.screenX + ' x ' + event.screenY);
         console.log('Clicked on (page): ' + event.pageX + ' x ' + event.pageY);
