b18831a5101b6b4f78bfcddd0c819a141e15deb2
Liridon <vondony@hotmail.com>
2020-02-20 02:18:56 +0100

    Small change to welcome message

--- work/s04/a04/route/slash.js
+++ work/s04/a04/route/slash.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 // Add a route for the path /
 router.get('/', (req, res) => {
-    res.send("Welcome to Assignment 4: please type /lotto or /report.html on the URL");
+    res.send("Welcome to Assignment 4: please add /lotto or /report.html at the end of the URL");
 });
 
 module.exports = router;
